3b210303936d06216d9ccc007984afa72d8e6ce7
Ahmad Fahmi <afahmi3@yahoo.co.id>
2017-11-21 20:11:09 +0700

    Update cyk_modified.c

--- cyk_modified.c
+++ cyk_modified.c
@@ -12,17 +12,12 @@ int prodNum, NInput, LENGTH;
 char Input[ROW_SIZE][MAX_SIZE];
 char currInputArray[MAX_SIZE][MAX_SIZE];
 char Gram[ROW_SIZE][COL_SIZE][MAX_SIZE];
+int tokenList[ROW_SIZE][COL_SIZE];
 
 
 void break_grammar(int row, char a[20]){ //separate LHS and RHS and insert them to the matrix
 	int i, idx = 0;
-	char *token, LHS[20], prodSplit[MAX_SIZE][MAX_SIZE];
-
-	/* Separate between LHS and RHS */
-	/*
-	S -> PROGRAM_START PROGRAM_BODY | PROGRAM_START BODY
-	S,->,PROGRAM_START,PROGRAM_BODY,|,PROGRAM_START,BODY
-	*/
+	char *token;
 
 	token = strtok(a, " ");
 	while(token != NULL){
@@ -34,28 +29,95 @@ void break_grammar(int row, char a[20]){ //separate LHS and RHS and insert them 
 	}
 }
 
-void break_input(char a[20], int *N){ //separate LHS and RHS and insert them to the matrix
-	int i, idx = 0;
-	char *token, LHS[20], prodSplit[MAX_SIZE][MAX_SIZE];
-	bool isSemicolon=false, isPeriod=false;
-
-	token = strtok(a, " ");
-	while(token != NULL){
-		if(strcmp(token, "0"))
-		strcpy(currInputArray[idx], token);
-		idx++;
-		token = strtok(NULL, " ");
-	}
-	*N = idx;
-}
-
-int checkToken(int i){
+int checkToken(char isi[MAX_SIZE]){
 	int j;
 
 	for(j=0; j<prodNum; j++)
-		if(strcmp(currInputArray[i], Gram[j][1])==0) return j;
+		if(strcmp(isi, Gram[j][1])==0) return j;
 
 	return -1;
+}
+
+void break_input(char a[20], int row, int *N){ //separate LHS and RHS and insert them to the matrix
+	int i, j, k, idx = 0, tokenIndex, tokenPerRow;
+	char *token, token_two[MAX_SIZE];
+	bool isSemicolon=false, isPeriod=false;
+
+	token = strtok(a, " ");
+	tokenPerRow = 1;
+
+	while(token != NULL){
+		char temp[MAX_SIZE], currTemp[2], nextTemp[2];
+
+		for(i=0; i<strlen(token); i++){
+			sprintf(currTemp, "%c", token[i]);
+
+			if(isdigit(token[i]) || isalpha(token[i])){ //while alphanumeric
+				
+				strcat(temp, currTemp);
+
+				if(!isdigit(token[i+1]) && !isalpha(token[i+1])){
+
+					/* Check whether it is number or not */
+					bool isNum=true;
+					for(j=0; j<strlen(temp); j++){
+						if(!isdigit(temp[j])) isNum=false;
+					}
+
+					/* Parse again */
+					if(!isNum){ //if alphanumeric
+						tokenIndex = checkToken(temp);
+						//printf("token %d ",tokenIndex);
+						if(tokenIndex == -1){ //if word
+							strcpy(currInputArray[idx], "word");
+							idx++;
+							tokenList[row][tokenPerRow] = 1;
+							tokenPerRow++;
+							//printf("token %s ", temp);
+						}else{ //if not word
+							strcpy(currInputArray[idx], temp);
+							idx++;
+							tokenList[row][tokenPerRow] = tokenIndex;
+							tokenPerRow++;
+							//printf("token %s ", temp);
+						}
+					}else{ //if number
+						strcpy(currInputArray[idx], "constant");
+						idx++;
+						tokenList[row][tokenPerRow] = 40;
+						tokenPerRow++;
+						//printf("token %s ", temp);
+					}
+
+					strcpy(temp, "");
+
+				}
+
+			}else{
+				//printf("currTemp %s ",currTemp);
+				tokenIndex = checkToken(currTemp);
+				if(tokenIndex != -1){ //if found
+					strcpy(currInputArray[idx], currTemp);
+					idx++;
+					tokenList[row][tokenPerRow] = tokenIndex;
+					tokenPerRow++;
+					//printf("token %s ", currTemp);
+				}else{ //if not found
+					strcpy(currInputArray[idx], "NOT_FOUND");
+					idx++;
+					tokenList[row][tokenPerRow] = -1;
+					tokenPerRow++;
+					//printf("token NOT_FOUND");
+				}
+			}
+		}
+
+		token = strtok(NULL, " ");
+	}
+
+	tokenList[row][0] = tokenPerRow;
+	*N = idx;
+	printf("\n");
 }
 
 
@@ -102,26 +164,9 @@ int main(){
 		strcpy(currInput, Input[h]);
 		LENGTH = strlen(Input[h]); //Length of current input line
 		memset(currInputArray, 0, sizeof currInputArray); //Set array to empty
-		break_input(Input[h], &num_of_input);
-
-		for(i=0; i<num_of_input; i++){
-			int idxToken;
-
-			idxToken = checkToken(i);
-			if(idxToken != -1) printf("<%s>",Gram[idxToken][1]);
-			else{
-
-				/* Check whether it is categorized as "word" or "constant" */
-				bool isNum=true;
-				for(j=0; j<strlen(currInputArray[i]); j++){
-					if(isalpha(currInputArray[i][j])) isNum = false;
-				}
-				if(!isNum) strcpy(currInputArray[i], "word");
-				else if(isNum) strcpy(currInputArray[i], "constant");
-
-				idxToken = checkToken(i);
-				if(idxToken != -1) printf("<%s>",Gram[idxToken][1]);
-			}
+		break_input(Input[h], h, &num_of_input);
+		for(i=1; i<tokenList[h][0]; i++){
+			printf("<%d>",tokenList[h][i]+1);
 		}
 		printf("\n");
 	}
