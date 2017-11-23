56651ea4c06bba050e74d85701fc9e9d9abb205c
Ahmad Fahmi <afahmi3@yahoo.co.id>
2017-11-23 16:38:12 +0700

    Update cyk_modified.c

--- cyk_modified.c
+++ cyk_modified.c
@@ -7,38 +7,53 @@
 #define COL_SIZE 50
 #define MAX_SIZE 500
 
+typedef struct{
+	int tknIdx;
+	char term[100];
+}TERMINAL, NONTERMINAL, GRAMMAR;
+
 /* Global variables */
-int prodNum, NInput, LENGTH;
+int prodNum, NInput, LENGTH, NTSize, TSize;
 char Input[ROW_SIZE][MAX_SIZE];
 char currInputArray[MAX_SIZE][MAX_SIZE];
-char Gram[ROW_SIZE][COL_SIZE][MAX_SIZE];
-int tokenList[ROW_SIZE][COL_SIZE];
+GRAMMAR Gram[ROW_SIZE][COL_SIZE];
+TERMINAL terminalList[ROW_SIZE];
+NONTERMINAL nonterminalList[ROW_SIZE];
+int tokenInput[ROW_SIZE][COL_SIZE];
 
+int checkToken(char isi[MAX_SIZE]){ //Check if input available in token
+	int i;
 
-void break_grammar(int row, char a[20]){ //separate LHS and RHS and insert them to the matrix
-	int i, idx = 0;
+	/* Check in nonterminal list */
+	for(i=0; i<NTSize; i++){
+		if(strcmp(nonterminalList[i].term, isi) == 0) return nonterminalList[i].tknIdx;
+	}
+
+	/* Check in terminal list */
+	for(i=0; i<TSize; i++){
+		if(strcmp(terminalList[i].term, isi) == 0) return terminalList[i].tknIdx;
+	}
+
+	return -1;
+}
+
+void break_grammar(int row, char *a){ //separate LHS and RHS and insert them to the matrix
+	int i, idx = 0, tokenIndex;
 	char *token;
 
 	token = strtok(a, " ");
 	while(token != NULL){
 		if((strcmp(token, "->")!=0) && (strcmp(token, "|")!=0)){
-			strcpy(Gram[row][idx], token);
+			Gram[row][idx].tknIdx = checkToken(token);
+			strcpy(Gram[row][idx].term, token);
 			idx++;
 		}
 		token = strtok(NULL, " ");
 	}
 }
 
-int checkToken(char isi[MAX_SIZE]){ //Check if input available in token
-	int i;
-
-	for(i=0; i<prodNum; i++)
-		if(strcmp(isi, Gram[i][1])==0) return i;
-
-	return -1;
-}
-
-void break_input(char a[20], int row, int *N){ //Parsing input
+/* JANGAN DIOTAK ATIK */
+void break_input(char a[20], int row){ //Parsing input
 	int i, j, k, idx, tokenIndex;
 	char *token;
 
@@ -47,6 +62,7 @@ void break_input(char a[20], int row, int *N){ //Parsing input
 
 	while(token != NULL){
 		char temp[MAX_SIZE], currTemp[2], nextTemp[2];
+		strcpy(temp, "");
 
 		for(i=0; i<strlen(token); i++){
 			sprintf(currTemp, "%c", token[i]);
@@ -63,21 +79,24 @@ void break_input(char a[20], int row, int *N){ //Parsing input
 						if(!isdigit(temp[j])) isNum=false;
 					}
 
+					//printf("- %s ",temp);
+
 					/* Parse again */
 					if(!isNum){ //if alphanumeric
 						tokenIndex = checkToken(temp);
 						if(tokenIndex == -1){ //if word
 							strcpy(currInputArray[idx], "word");
-							tokenList[row][idx] = checkToken("word");
+							tokenInput[row][idx] = checkToken("word");
 							idx++;
 						}else{ //if not word
+							//printf("- %s ",temp);
 							strcpy(currInputArray[idx], temp);
-							tokenList[row][idx] = tokenIndex;
+							tokenInput[row][idx] = tokenIndex;
 							idx++;
 						}
 					}else{ //if number
 						strcpy(currInputArray[idx], "constant");
-						tokenList[row][idx] = checkToken("constant");
+						tokenInput[row][idx] = checkToken("constant");
 						idx++;
 					}
 
@@ -90,11 +109,11 @@ void break_input(char a[20], int row, int *N){ //Parsing input
 				tokenIndex = checkToken(currTemp);
 				if(tokenIndex != -1){ //if found
 					strcpy(currInputArray[idx], currTemp);
-					tokenList[row][idx] = tokenIndex;
+					tokenInput[row][idx] = tokenIndex;
 					idx++;
 				}else{ //if not found
 					strcpy(currInputArray[idx], "NOT_FOUND");
-					tokenList[row][idx] = -999;
+					tokenInput[row][idx] = -999;
 					idx++;
 				}
 			}
@@ -103,16 +122,15 @@ void break_input(char a[20], int row, int *N){ //Parsing input
 		token = strtok(NULL, " ");
 	}
 
-	tokenList[row][0] = idx;
-	*N = idx-1;
+	tokenInput[row][0] = idx;
 	printf("\n");
 }
 
 
 int main(){
 	int h, i, j, k, l, m, n;
-	char start, prod[MAX_SIZE], strInput[MAX_SIZE], tmp[2], copy[10];
-	FILE *fileInput;
+	char start, prod[MAX_SIZE], strInput[MAX_SIZE], tmp[2], copy[10], TERM[MAX_SIZE];
+	FILE *fileInput, *fileTerminal, *fileNonTerminal;
 
 	printf("Tubes TBFO II : 2nd Milestone - THE TOKENIZER\n");
 	printf("             ---- Created by : ----          \n");
@@ -121,9 +139,36 @@ int main(){
 	printf("------   Adylan Roaffa Ilmy - 13516016 ------\n");
 	printf("---------------------------------------------\n\n");
 
-	printf("\nCFG (Terminals only) :\n\n");
+	/* Tokenize the terminal */
+	fileTerminal = fopen("terminal.txt", "r");
+	i = 0;
+	int currToken = 1;
+	while(!feof(fileTerminal)){
+		fgets(TERM, MAX_SIZE, fileTerminal);
+		strtok(TERM, "\n");
+		terminalList[i].tknIdx = currToken;
+		strcpy(terminalList[i].term, TERM);
+		currToken++;
+		i++;
+	}
+	TSize = currToken - 1;
+
+	/* Tokenize the nonterminal */
+	fileNonTerminal = fopen("nonterminal.txt", "r");
+	i = 0;
+	while(!feof(fileNonTerminal)){
+		fgets(TERM, MAX_SIZE, fileNonTerminal);
+		strtok(TERM, "\n");
+		nonterminalList[i].tknIdx = currToken;
+		strcpy(nonterminalList[i].term, TERM);
+		currToken++;
+		i++;
+	}
+	NTSize = currToken - 1;
+
+	printf("\nCFG :\n\n");
 	prodNum = 0;
-	fileInput = fopen("input.txt", "r");
+	fileInput = fopen("grammar.txt", "r");
 	while(!feof(fileInput)){
 		fgets(prod, MAX_SIZE, fileInput);
 		strtok(prod, "\n");
@@ -132,12 +177,10 @@ int main(){
 		prodNum++;
 	}
 
-	/* Print token of productions */
-	printf("\nToken :\n\n");
 	for(i=0; i<prodNum; i++){
-		printf("<%d> ",i+1);
-		for(j=1; j<3; j++){
-			printf("%s ", Gram[i][j]);
+		for(j=0; j<10; j++){
+			if(Gram[i][j].tknIdx != 0)
+				printf("<%d> ", Gram[i][j].tknIdx);
 		}
 		printf("\n");
 	}
@@ -152,18 +195,12 @@ int main(){
 		strtok(Input[i], "\n");
 	}
 
-	/* Tokenizer */
+	/* Convert input strings into tokens */
 	for(h=0; h<NInput; h++){
-		/* Initialize */
-		char currInput[100];
-		int num_of_input;
-
-		strcpy(currInput, Input[h]);
-		LENGTH = strlen(Input[h]); //Length of current input line
 		memset(currInputArray, 0, sizeof currInputArray); //Set array to empty
-		break_input(Input[h], h, &num_of_input);
-		for(i=1; i<tokenList[h][0]; i++){
-			printf("<%d>",tokenList[h][i]+1);
+		break_input(Input[h], h);
+		for(i=1; i<tokenInput[h][0]; i++){
+			printf("<%d>",tokenInput[h][i]);
 		}
 		printf("\n");
 	}
